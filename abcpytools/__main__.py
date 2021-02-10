12d7443f46576d6720818d04cceb034d6f60d03b
PiRK <pierrekn@gmail.com>
2021-02-10 15:06:53 +0100

    add buildavalancheproof command
    Make the main script parse the command line, take the first argument as
    the command to execute (currently only 'buildavalancheproof'),
    specify the command line arguments for buildavalancheproof.
    
    Test Plan:
    ```
    python -m abcpytools  buildavalancheproof 42 1699999999 030b4c866585dd868a9d62348a9cd008d6a312937048fff31670e7e920cfc7a744 '[{"txid": "0adfaf4927b0d5482a3f01ac508274d6a418fbc60f3e45213870435f3f8a28a8", "vout": 0, "amount": 0.001, "height": 665883, "privatekey": "L..."}]'
    ```

--- abcpytools/__main__.py
+++ abcpytools/__main__.py
@@ -1,28 +1,64 @@
-# This is a sample Python script.
+"""Main entry point for the package"""
+import argparse
+import json
+import sys
+
 from abcpytools.proof import PublicKey, ProofBuilder
 
 
-def main():
-    pubkey = PublicKey(bytes.fromhex(
-        "030b4c866585dd868a9d62348a9cd008d6a312937048fff31670e7e920cfc7a744"))
-    proofbuilder = ProofBuilder(sequence=42,
-                                expiration_time=1699999999,
+def buildavalancheproof(argv):
+    parser = argparse.ArgumentParser(
+            description="Build a proof for avalanche's sybil resistance")
+
+    parser.add_argument(
+        "sequence", type=int, help="The proof's sequence")
+    parser.add_argument(
+        "expiration", type=int,
+        help="A timestamp indicating when the proof expires")
+    parser.add_argument(
+        "master", type=str, help="The master public key")
+    parser.add_argument(
+        "stakes", type=json.loads,
+        help="The stakes to be signed and associated private keys, as a json "
+             "list of objects. A stake is defined by the following attributes:"
+             " 'txid' the transaction id, a 32 character hexadecimal string;"
+             " 'vout' the utxo's output index;"
+             " 'amount' the utxo's amount, in bitcoins;"
+             " 'height' the height of the block containing the transaction;"
+             " 'privatekey' the private key unlocking the output, in WIF format.")
+
+    args = parser.parse_args(argv)
+    pubkey = PublicKey(bytes.fromhex(args.master))
+    proofbuilder = ProofBuilder(sequence=args.sequence,
+                                expiration_time=args.expiration,
                                 master=pubkey)
-    proofbuilder.add_utxo(
-        txid="0adfaf4927b0d5482a3f01ac508274d6a418fbc60f3e45213870435f3f8a28a8",
-        vout=0,
-        value=0.001,
-        height=665883,
-        wif_privkey="...")
+    for utxo in args.stakes:
+        proofbuilder.add_utxo(
+            txid=utxo['txid'],
+            vout=utxo['vout'],
+            value=utxo['amount'],
+            height=utxo['height'],
+            wif_privkey=utxo['privatekey'])
     proof = proofbuilder.build()
-    hex_str = proof.serialize().hex()
+    print(proof.serialize().hex())
 
-    expected = "2a00000000000000fff053650000000021030b4c866585dd868a9d62348a9cd008d6a312937048fff31670e7e920cfc7a74401a8288a3f5f43703821453e0fc6fb18a4d6748250ac013f2a48d5b02749afdf0a00000000a086010000000000365214002102000be4ace4549fda67998ddf2854051878901f31aa9392bfaf1af22015724a36c0df0e9f7e21de384cbff3bde497b9228ecd23412a2210212d1b4271f35794e3b76236faa60e7fa0b960c247f4a9e36d538cb92f9e57900c3af4b020b411a668"
-    #           ^ sequence      ^ exp_time      ^ master pubkey                                                     ^ ^ txid                                                          ^ vout  ^ amount        ^ h     ^ pubkey                                                            ^ sig
 
-    assert hex_str == expected, "building the proof failed"
-    print("Proof successfully generated")
+def main():
+    parser = argparse.ArgumentParser(
+            description="Bitcoin toolkit")
+    commands = ["buildavalancheproof"]
+    parser.add_argument(
+        'command',
+        help=f'Command to execute.\nAvailable commands: {", ".join(commands)}')
+    args = parser.parse_args(sys.argv[1:2])
+
+    if args.command == "buildavalancheproof":
+        buildavalancheproof(sys.argv[2:])
+    else:
+        print(f"Unknown command {args.command}")
+        sys.exit(1)
 
 
 if __name__ == '__main__':
     main()
+    sys.exit(0)
